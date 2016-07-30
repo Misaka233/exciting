70b2aa09eb9bcf9978c7ef7b6e3a5273a6c69d19
Misaka233 <1224936828@qq.com>
2016-07-30 16:50:25 +0800

    exciting
    exciting

--- LRU.cpp
+++ LRU.cpp
@@ -1,49 +1,44 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-const int maxn = 2e5+10;
-int cy[maxn];
-int fa[maxn];
-int Find(int x){
-    return fa[x] == x ? x : fa[x] = Find(fa[x]);
-}
-int cnt = 0;
-int tmp = 0;
-int Union(int x ,int y){
-    int a = Find(x);
-    int b = Find(y);
-    if(a == b){
-        cy[x] = 0;
-        if(x == y ) tmp = x;
-        cnt ++;
-    }
-    else cy[x] = y;
-    fa[b] = a;
-}
-int main(){
-    int n,t;
-    cin >>n;
-    for(int i = 0 ; i <= n ; i ++){
-        fa[i] = i;
-    }
-    int f = 0;
-    for(int i = 0 ; i < n ; i ++){
-        scanf("%d",&t);
-        Union(i+1,t);
-        if(t == i+1)
-            f = 1;
-    }
-
-    printf("%d\n",cnt - f);
-    for(int i = 1 ; i <= n ; i ++){
-        if(cy[i])
-            printf("%d ",cy[i]);
-        else if(tmp)
-            printf("%d ",tmp);
-        else{
-            tmp = i;
-            printf("%d ",tmp);
-        }
-    }
+//×´Ì¬Ñ¹Ëõ
+#include<stdio.h>
+int n,m;
+double p[22];
+double ans[22];
+double dp[1<<20];
+int main()
+{
+	int i,j,k;
+	double sp;
+	scanf("%d%d",&n,&m);
+	for(i=0;i<n;i++)
+    {
+		scanf("%lf",&p[i]);
+	}
+	dp[0]=1;
+	for(i=1;i<(1<<n);i++)
+	{
+		sp=0;
+		k=0;
+		for(j=0;j<n;j++)
+		{
+			if(((1<<j)&i)==0)
+			{
+				sp+=p[j];
+				k++;
+			}
+		}
+		if(n-k>m)continue;
+		for(j=0;j<n;j++){
+			if(p[j]<0.000000001)continue;
+			if(((1<<j)&i)!=0)
+			{
+				dp[i]+=dp[i-(1<<j)]*p[j]/(sp+p[j]);
+				ans[j]+=dp[i-(1<<j)]*p[j]/(sp+p[j]);
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%.10lf ",ans[i]);
+	}
+	return 0;
 }
